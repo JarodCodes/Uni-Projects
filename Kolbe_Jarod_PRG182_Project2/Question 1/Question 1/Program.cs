0ffc92a5a5637a7d0503b872d72021918768236b
Jarod <JarodGeorgeKolbe@gmail.com>
2022-05-25 14:03:43 +0200

    PRG182 Project2

---
+++ Kolbe_Jarod_PRG182_Project2/Question 1/Question 1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>();
+            int option;
+            bool done = false;
+
+            words = InitList();
+
+            while (done == false)
+            {
+                option = GetMenu(words);
+                if(option == words.Count)
+                {
+                    done = true;
+                }
+                else
+                {
+                    GetVowels(words, option);
+                }
+            }
+        }
+        static public List<string> InitList()
+        {
+            List<string> words = new List<string>();
+            Console.WriteLine("Insert 10 words:\n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                words.Add(Console.ReadLine());
+            }
+
+            words.Sort();
+            return words;
+        }
+        static public int GetMenu(List<string> words)
+        {
+            Console.WriteLine("\nSelect a word:\n");
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                Console.WriteLine("{0}. {1}", i+1, words[i]);
+            }
+            Console.WriteLine("{0}. to exit", words.Count + 1);
+
+            return int.Parse(Console.ReadLine()) - 1;
+        }
+        static public void GetVowels(List<string> words, int option)
+        {
+            string word = words[option];
+            int a = 0, e = 0, i = 0, o = 0, u = 0;
+
+            foreach(var item in word.ToLower())
+            {
+                switch (item)
+                {
+                    case 'a':
+                        a++;
+                        break;
+                    case 'e':
+                        e++;
+                        break;
+                    case 'i':
+                        i++;
+                        break;
+                    case 'o':
+                        o++;
+                        break;
+                    case 'u':
+                        u++;
+                        break;
+                }
+            }
+
+            Console.WriteLine("\nWord chosen is {0}\na = {1}\ne = {2}\ni = {3}\no = {4}\nu = {5}", word, a, e, i, o, u);
+        }
+    }
+}
---
+++ Kolbe_Jarod_PRG182_Project2/Question 2/Question 2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool done = false;
+            string account;
+
+            while(done == false)
+            {
+                account = GetAccount();
+                if(account == "x")
+                {
+                    done = true;
+                }
+                else
+                {
+                    DetermineResult(account);
+                }
+            }
+        }
+        static public string GetAccount()
+        {
+            Console.Write("Enter type of account (s for Savings, or c for Cheque): ");
+
+            return Console.ReadLine().ToLower();
+        }
+        static public void DetermineResult(string account)
+        {
+            double balance, min, interest;
+            Console.Write("\nEnter minimum balance: ");
+            min = double.Parse(Console.ReadLine());
+            Console.Write("\nEnter current balance of customer: ");
+            balance = double.Parse(Console.ReadLine());
+
+            if(balance < min)
+            {
+                if(account == "s")
+                {
+                    Console.WriteLine("\nService charge of R50.00 must be paid");
+                    balance -= 50;
+                    Console.WriteLine("Final balance = {0}", balance);
+                }
+                else
+                {
+                    Console.WriteLine("\nService charge of R125.00 must be paid");
+                    balance -= 125;
+                    Console.WriteLine("Final balance = {0}", balance);
+                }
+            }
+            else
+            {
+                if (account == "s")
+                {
+                    interest = balance * 0.04;
+                    balance += interest;
+                    Console.WriteLine("\nInterest = R{0}", interest);
+                    Console.WriteLine("\nFinal balance = R{0}\n", balance);
+                }
+                else if(balance > min + 10000)
+                {
+                    interest = balance * 0.03;
+                    balance += interest;
+                    Console.WriteLine("\nInterest = R{0}", interest);
+                    Console.WriteLine("\nFinal balance = R{0}\n", balance);
+                }
+                else
+                {
+                    interest = balance * 0.05;
+                    balance += interest;
+                    Console.WriteLine("\nInterest = R{0}", interest);
+                    Console.WriteLine("\nFinal balance = R{0}\n", balance);
+                }
+            }
+        }
+
+    }
+}
